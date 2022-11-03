6ad41bfc733cd0c0f2bcccdbfc876823ad84f524
AbdellaIbrahim <zeref@Abdellas-MacBook-Pro.local>
2022-11-03 11:26:34 -0400

    MembersDao work

--- src/main/java/Warden/Members/MembersDao.java
+++ src/main/java/Warden/Members/MembersDao.java
@@ -1,13 +1,36 @@
 package Warden.Members;
 
+import Warden.ConnectionPooling.ConnectionManager;
 import Warden.Dao.Dao;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
+
+import static Warden.Main.Driver.getMyLogger;
 
 public class MembersDao implements Dao<Member> {
     @Override
     public Optional<Member> get(long id) {
+        String statement = "SELECT * FROM members where id = ?";
+        try{
+            Connection connection = ConnectionManager.getConnection();
+            ResultSet resultSet = connection.prepareStatement(statement).executeQuery();
+            if(resultSet.next()){
+                Member member = new Member(
+                        resultSet.getLong("member_id")
+                );
+                member.setBans(resultSet.getInt("bans"));
+                member.setStrikes(resultSet.getInt("strikes"));
+                member.setWarnings(resultSet.getInt("warnings"));
+                return Optional.of(member);
+            }
+        }catch (SQLException e){
+            getMyLogger().error("Unable to prepare statement, Check syntax please.");
+
+        }
         return Optional.empty();
     }
 
